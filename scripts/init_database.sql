862df8f0e57f5f7cf835254e5333a8296ef8cee6
Ab3l-Dev <129977060+Ab3l-Dev@users.noreply.github.com>
2025-03-19 09:34:56 -0300

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,16 @@
+-- Create the DataWarehouse database
+CREATE DATABASE DataWarehouse;
+GO
+  
+USE DataWarehouse;
+GO
+  
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver; 
+GO /* GO in SQL is like a separator*/
+
+CREATE SCHEMA gold;
+GO
 
